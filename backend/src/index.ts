210a45149034de638a9b0a4b4a153ffd710a34ea
aaronfllr <aaronfllr@gmail.com>
2024-09-17 00:46:44 -0400

    Recieving Access and Refresh tokens from Strava

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import cors from 'cors'
 import express from 'express'
+import authRoutes from './routes/auth'
 
 const prisma = new PrismaClient()
 const app = express()
@@ -8,9 +9,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/auth', authRoutes) 
+
 app.use('/', (req, res) => {
   res.send('Hello World S!')
 })
+
+
 
 const server = app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
