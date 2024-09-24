eef150f3c0c0db3d336e819b41e7137de1b8eefb
aaronfllr <aaronfllr@gmail.com>
2024-09-23 20:22:58 -0400

    Update home page content

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -2,8 +2,8 @@ export default function Home() {
     return (
         <>
             <div>
-                <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">Welcome to TailAdmin</h1>
-                <p className="mt-4 text-gray-600 dark:text-gray-300">TailAdmin is a free and open-source Tailwind CSS admin dashboard template. It is a modern and clean admin template built with Tailwind CSS. It is fully responsive and works on all modern web browsers.</p>
+                <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">Welcome to a work in progress!</h1>
+                <p className="mt-4 text-gray-600 dark:text-gray-300">This should really have a snazzy home page, I'll get to it once I finish the Auth</p>
             </div>
         </>
     )
